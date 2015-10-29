2c2af3384f394adc11617e4e9fcbb12313973fac
Forrest Yang <yforrest@qq.com>
2015-10-29 11:33:30 +0800

    add ios 9.1 Shortcut Icon

--- src/ios/app/ThreeDeeTouch.m
+++ src/ios/app/ThreeDeeTouch.m
@@ -58,13 +58,35 @@
 }
 
 - (UIApplicationShortcutIconType) UIApplicationShortcutIconTypeFromString:(NSString*)str {
-       if ([str isEqualToString:@"compose"])  return UIApplicationShortcutIconTypeCompose;
-  else if ([str isEqualToString:@"play"])     return UIApplicationShortcutIconTypePlay;
-  else if ([str isEqualToString:@"pause"])    return UIApplicationShortcutIconTypePause;
-  else if ([str isEqualToString:@"add"])      return UIApplicationShortcutIconTypeAdd;
-  else if ([str isEqualToString:@"location"]) return UIApplicationShortcutIconTypeLocation;
-  else if ([str isEqualToString:@"search"])   return UIApplicationShortcutIconTypeSearch;
-  else if ([str isEqualToString:@"share"])    return UIApplicationShortcutIconTypeShare;
+       if ([str isEqualToString:@"Compose"])  return UIApplicationShortcutIconTypeCompose;
+  else if ([str isEqualToString:@"Play"])     return UIApplicationShortcutIconTypePlay;
+  else if ([str isEqualToString:@"Pause"])    return UIApplicationShortcutIconTypePause;
+  else if ([str isEqualToString:@"Add"])      return UIApplicationShortcutIconTypeAdd;
+  else if ([str isEqualToString:@"Location"]) return UIApplicationShortcutIconTypeLocation;
+  else if ([str isEqualToString:@"Search"])   return UIApplicationShortcutIconTypeSearch;
+  else if ([str isEqualToString:@"Share"])    return UIApplicationShortcutIconTypeShare;
+  else if ([str isEqualToString:@"Prohibit"]) return UIApplicationShortcutIconTypeProhibit;
+  else if ([str isEqualToString:@"Contact"])  return UIApplicationShortcutIconTypeContact;
+  else if ([str isEqualToString:@"Home"])     return UIApplicationShortcutIconTypeHome;
+  else if ([str isEqualToString:@"MarkLocation"])    return UIApplicationShortcutIconTypeMarkLocation;
+  else if ([str isEqualToString:@"Favorite"]) return UIApplicationShortcutIconTypeFavorite;
+  else if ([str isEqualToString:@"Love"])     return UIApplicationShortcutIconTypeLove;
+  else if ([str isEqualToString:@"Cloud"])    return UIApplicationShortcutIconTypeCloud;
+  else if ([str isEqualToString:@"Invitation"])      return UIApplicationShortcutIconTypeInvitation;
+  else if ([str isEqualToString:@"Confirmation"])    return UIApplicationShortcutIconTypeConfirmation;
+  else if ([str isEqualToString:@"Mail"])     return UIApplicationShortcutIconTypeMail;
+  else if ([str isEqualToString:@"Message"])  return UIApplicationShortcutIconTypeMessage;
+  else if ([str isEqualToString:@"Date"])     return UIApplicationShortcutIconTypeDate;
+  else if ([str isEqualToString:@"Time"])     return UIApplicationShortcutIconTypeTime;
+  else if ([str isEqualToString:@"CapturePhoto"])    return UIApplicationShortcutIconTypeCapturePhoto;
+  else if ([str isEqualToString:@"CaptureVideo"])    return UIApplicationShortcutIconTypeCaptureVideo;
+  else if ([str isEqualToString:@"Task"])     return UIApplicationShortcutIconTypeTask;
+  else if ([str isEqualToString:@"TaskCompleted"])   return UIApplicationShortcutIconTypeTaskCompleted;
+  else if ([str isEqualToString:@"Alarm"])    return UIApplicationShortcutIconTypeAlarm;
+  else if ([str isEqualToString:@"Bookmark"]) return UIApplicationShortcutIconTypeBookmark;
+  else if ([str isEqualToString:@"Shuffle"])  return UIApplicationShortcutIconTypeShuffle;
+  else if ([str isEqualToString:@"Audio"])    return UIApplicationShortcutIconTypeAudio;
+  else if ([str isEqualToString:@"Update"])   return UIApplicationShortcutIconTypeUpdate;
   else {
     NSLog(@"Invalid iconType passed to the 3D Touch plugin.");
     return 0;
